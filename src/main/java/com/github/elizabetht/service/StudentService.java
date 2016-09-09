354eb3a53c2eabcd796ae4f071bc36f1d681f6d8
vimpal <vimpal.singh@gmail.com>
2016-09-09 15:33:15 +0530

    Update StudentService.java
    test

--- src/main/java/com/github/elizabetht/service/StudentService.java
+++ src/main/java/com/github/elizabetht/service/StudentService.java
@@ -3,6 +3,7 @@ package com.github.elizabetht.service;
 import com.github.elizabetht.model.StudentInterface;
 
 public interface StudentService {
+	//test
 	StudentInterface save(StudentInterface student);
 	boolean findByLogin(String userName, String password);
 	boolean findByUserName(String userName);
